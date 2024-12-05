6bf6f29bd05356f9355403094891bdb48f3ee74e
Aaron <nadelman94@gmail.com>
2024-12-05 16:43:04 -0300

    SettingsDone

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./assets/svg/ButtonGradient";
 
 interface AppProps {
-  name?: string;
+  name?: "string";
 }
 
 const App: React.FC<AppProps> = ({ name }) => {
   return (
     <div>
       <h1>Hello, {name}!</h1>
-      <Button></Button>
       <ButtonGradient />
     </div>
   );
