b13c0daefdcdea8befd84f2859d8a3bbcf9185a8
Aaron <nadelman94@gmail.com>
2024-12-12 21:47:10 -0300

    animation

--- src/components/sobre-mi-hero/SobreMiHero.tsx
+++ src/components/sobre-mi-hero/SobreMiHero.tsx
@@ -3,12 +3,12 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 const SobreMiHero = () => {
   return (
     <div>
-      <section className="bg-gradient-to-r from-purple-500 to-pink-500 mx-20 my-20 rounded-3xl shadow-xl py-1 sm:py-16 lg:py-24 font-body">
+      <section className="bg-gradient-to-r from-purple-500 to-pink-500 mx-20 my-20 sm:mx-1 sm:my-1 rounded-3xl shadow-xl py-1 sm:py-16 lg:py-24 font-body">
         <div className="pl-1 pr-4 mx-auto max-w-7xl sm:pl-1 sm:pr-4 lg:pl-1 lg-pr-8  ">
           <div className="grid items-start grid-cols-1 lg:grid-cols-2 gap-x-12 xl:gap-x-24 gap-y-12">
             <div className="relative lg:mb-12">
               <div className="my-5 mx-5">
-                <DotLottieReact src="/codingAvatar.lottie" loop autoplay />
+                <DotLottieReact src="./codingAvatar.lottie" loop autoplay />
               </div>
             </div>
 
