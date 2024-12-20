b8797c79888f56fe367e021f59e58d9d176cff45
Aaron <nadelman94@gmail.com>
2024-12-20 04:46:05 -0300

    Form 1

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,32 @@
 function Footer() {
   return (
     <div>
-      <footer className="footer justify-center text-neutral-content p-10">
+      <footer className="footer stiky bottom-0 justify-center text-neutral-content p-10">
         <nav>
           <h6 className="footer-title">Social</h6>
           <div className="grid grid-flow-col gap-4">
-            <a>
+            <a href="https://github.com/AaronNadelman" target="_blank">
               <img
                 className="max-w-12 mx-5"
                 src="./Socials/github.png"
-                alt=""
+                alt="Git Hub logo"
               />
             </a>
-            <a>
-              <img className="max-w-12 mx-5" src="./Socials/gmail.png" alt="" />
+            <a href="nadelman94@gmail.com" target="_blank">
+              <img
+                className="max-w-12 mx-5"
+                src="./Socials/gmail.png"
+                alt="Gmail logo"
+              />
             </a>
-            <a>
+            <a
+              href="https://www.linkedin.com/in/aaron-dario-nadelman"
+              target="_blank"
+            >
               <img
                 className="max-w-12 mx-5"
                 src="./Socials/linkedin.png"
-                alt=""
+                alt="Linkedin logo"
               />
             </a>
           </div>
--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -1,5 +1,96 @@
-const Contacto = () => {
-  return <div>Contacto</div>;
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import React, { useState } from "react";
+
+interface FormData {
+  name: string;
+  subject: string;
+  location: string;
+  companyName?: string;
+  email: string;
+}
+
+const MyForm = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    subject: "",
+    location: "",
+    companyName: "",
+    email: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData); // Aquí puedes enviar los datos a un servidor
+  };
+
+  return (
+    <div className="flex-row">
+      <div className="mx-10">
+        <DotLottieReact src="./mainScene.lottie" autoplay loop />
+      </div>
+      <div className="flex flex-col items-center justify-center mx-10">
+        <h1 className="text-3xl font-bold mb-4">Formulario de Contacto</h1>
+        <p className="mb-8">Por favor, completa los siguientes campos.</p>
+        <form onSubmit={handleSubmit} className="w-full max-w-md">
+          <div className="mb-4">
+            <label htmlFor="name" className="blocktext-sm font-bold mb-2">
+              Nombre Completo
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Tu nombre"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-sm font-bold mb-2">
+              Asunto
+            </label>
+            <input
+              type="text"
+              id="asunto"
+              name="asunto"
+              value={formData.name}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Tu asunto"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formData.name}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="email"
+            />
+          </div>
+          {/* Resto de los campos (subject, location, companyName, email) con la misma estructura */}
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default Contacto;
+export default MyForm;
